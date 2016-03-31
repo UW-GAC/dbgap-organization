d5313c55a1a1eeabcfb15fc100bac4e683fb4377
Adrienne Stilp <amstilp@uw.edu>
2016-03-31 10:35:13 -0700

    A number of changes, see below:
    - update logic in get_file_type, shouldn't be any actual changes
    - add 'symlinked' field to dictionary returned by get_file_type
    - add get_var_report_match and get_data_dict_match functions to return
    the var_report file and data_dict file that go with a given 'phenotype'
    or 'special' file.
    - start make_symlinks function

--- organize_dbgap.py
+++ organize_dbgap.py
@@ -15,13 +15,20 @@ dbgap_re_dict = {'data_dict': r'^(?P<dbgap_id>phs\d{6}\.v\d+?\.pht\d{6}\.v\d+?)\
            'special': r'^(?P<dbgap_id>phs\d{6}\.v\d+?\.pht\d{6}\.v\d+?)\.p(\d+?)\.(.+?)\.MULTI.txt$'
            }
 
+# some notes:
+# the var_reports and data dictionaries pertain to a single participant set number; they are the same across consent groups
+# we only need to link 1 var_report and 1 data_dict for each phenotype dataset.
+
 def get_file_type(basename, re_dict=dbgap_re_dict):
     for key, value in re_dict.items():
         match = re.match(value, basename)
-        if match:
-            return key
-    
-    return None
+        if match is not None:
+            file_type = key
+            break
+
+    return {'file_type': key,
+            'match': match,
+            'symlinked': False}
     
 def get_file_type_dict(directory):
     
@@ -30,20 +37,52 @@ def get_file_type_dict(directory):
         for name in files:
             # os.symlink!
             # os.relpath!
-            file_type = get_file_type(name)
+            results = get_file_type(name)
+            
             
             full_path = os.path.join(root, name)
             relative_path = os.path.relpath(full_path)
-            file_dict[relative_path] = file_type
+            file_dict[full_path] = results
             
     return file_dict
+
 
 def check_directory_structure():
     """This function will check structure of linked directories:
     ie all var_reports and data_dictionaries exist for a given phenotype file:"""
     pass
 
+def get_var_report_match(file_dict, key):
+    dbgap_id_to_match = file_dict[key]['match'].groupdict()['dbgap_id']
+    for k in file_dict.keys():
+        if file_dict[k]['file_type'] == 'var_report':
+            if file_dict[k]['match'] is not None:
+                if file_dict[k]['match'].groupdict()['dbgap_id'] == dbgap_id_to_match:
+                    return k
 
+
+def get_data_dict_match(file_dict, key):
+    dbgap_id_to_match = file_dict[key]['match'].groupdict()['dbgap_id']
+    for k in file_dict.keys():
+        if file_dict[k]['file_type'] == 'data_dict':
+            if file_dict[k]['match'] is not None:
+                if file_dict[k]['match'].groupdict()['dbgap_id'] == dbgap_id_to_match:
+                    return k
+
+
+def make_symlinks(directory):
+    file_dict = get_file_type_dict(directory)
+    
+    # loop over special files and link one of each, plus var_report and data_dictionary
+    subject_files = [f for f in file_dict.keys() if file_dict[f]['file_type'] == 'special' and 'Subject' in f]
+    pedigree_files = [f for f in file_dict.keys() if file_dict[f]['file_type'] == 'special' and 'Pedigree' in f]
+    sample_files = [f for f in file_dict.keys() if file_dict[f]['file_type'] == 'special' and 'Sample' in f]
+    
+    print('subject:\t', subject_files[0])
+    print('matching var report:\t', get_var_report_match(file_dict, subject_files[0]))
+    print('matching data dict:\t', get_data_dict_match(file_dict, subject_files[0]))
+    # loop over phenotype files and link all from each consent group, plus one var_report and one data_dictionary
+    
 if __name__ == '__main__':
     parser = ArgumentParser()
     
@@ -51,10 +90,14 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
-    file_types = get_file_type_dict(args.directory)
-    for key, value in file_types.items():
-        print(value, "\t", key)
-
+    make_symlinks(args.directory)
+    
+    #file_types = get_file_type_dict(args.directory)
+    #for key, value in file_types.items():
+    #    if value['match'] is not None:
+    #        print(value['file_type'], '\t', value['match'].groupdict(), "\t", key)
+    #    else:
+    #        print('None', '\t', os.path.basename(key))
 
 """    
 Subjects
